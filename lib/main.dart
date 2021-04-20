71e7f1a5dba46a2f4c70444ab4e778c62493f273
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-20 16:32:01 -0500

    Iniciando El Proyecto Del Catalogo De Peliculas

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:catalogo_peliculas/src/pages/home_page.dart';
+
+void main(List<String> args) {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // centerTitle: false, No Es Necesaria, Pero Existe
+          title: Text('Peliculas En Cines'),
+          backgroundColor: Colors.indigoAccent,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity, // Utiliza Todo El Ancho Posible
+      height: 300.0,
+      child: new Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "https://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
+    );
+  }
+}
